17778ab361cdde2b22fb1be8ff6789e4ca9152a3
Jo <67304955+RE-Tamayo@users.noreply.github.com>
2022-09-01 23:35:32 +0800

    Change project structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series} = require("gulp");
 const sass = require('gulp-sass')(require("sass"));
 
 function buildStyles() {
-    return src('pixel.scss')
+    return src('pixel/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'));
 }
 
 function watchChanges() {
-    watch(['pixel.scss'], buildStyles);
+    watch(['pixel/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchChanges);
