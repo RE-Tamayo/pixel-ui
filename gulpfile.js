cb7fbd9cdafda90e66b6187456835684e0d99f3a
Jo <67304955+RE-Tamayo@users.noreply.github.com>
2022-09-01 13:41:55 +0800

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series} = require("gulp");
+const sass = require('gulp-sass')(require("sass"));
+
+function buildStyles() {
+    return src('pixel.scss')
+    .pipe(sass())
+    .pipe(dest('css'));
+}
+
+function watchChanges() {
+    watch(['pixel.scss'], buildStyles);
+}
+
+exports.default = series(buildStyles, watchChanges);
